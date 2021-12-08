15c4b2ed8363d09486ac5a73c65aa99f76551449
”umono“ <umono@umono.local>
2021-12-08 18:01:02 +0800

    ini

---
+++ sweet-cloud-module/sweet-cloud-base/sweet-cloud-nacos/src/main/java/org/sweet/boot/sweetcloudnacos/SweetCloudNacosApplication.java
@@ -0,0 +1,13 @@
+package org.sweet.boot.sweetcloudnacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetCloudNacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetCloudNacosApplication.class, args);
+	}
+
+}
---
+++ sweet-cloud-module/sweet-cloud-base/sweet-cloud-nacos/src/test/java/org/sweet/boot/sweetcloudnacos/SweetCloudNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sweet.boot.sweetcloudnacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetCloudNacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
